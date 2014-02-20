110e15b7b63d6a2aaa4826e30f08e5c6fcdd4ac2
harrisown <harrisown@gmail.com>
2014-02-19 21:38:28 -0500

    Changed from ECB to CBC with IV

--- CryptoHomework/src/CryptoTests.java
+++ CryptoHomework/src/CryptoTests.java
@@ -1,6 +1,9 @@
+import java.util.Random;
 import java.util.Scanner;
 import java.security.*;
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class CryptoTests {
@@ -12,6 +15,7 @@ public class CryptoTests {
 		String secret = sc.nextLine();
 		AEStest(secret);
 		RSAtest(secret);
+		sc.close();
 	}
 
 	private static void RSAtest(String secret)throws Exception{
@@ -46,18 +50,22 @@ public class CryptoTests {
 		System.out.println("");
 		System.out.println("Starting generation of AES key...");
 		Key key = GenerateSymmetricKey(192);
+		byte[] IV = new byte[16];
+		Random random = new SecureRandom();
+		random.nextBytes(IV);
+		
 		System.out.println("Finished generating AES key");
 
-		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
 		System.out.println("Starting encryption..");
-		cipher.init(Cipher.ENCRYPT_MODE, key); //encrypt using key
+		cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(IV)); //encrypt using key
 
 		byte[] cipherText = cipher.doFinal(plainText);
 		System.out.print("Finish encryption: ");
 		System.out.println(new String(cipherText, "UTF8")+ " is the encrypted message!");
 
 		System.out.println("Starting decryption...");
-		cipher.init(Cipher.DECRYPT_MODE, key);//decrypte using the same key!
+		cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(IV));//decrypte using the same key!
 		byte[] newPlainText = cipher.doFinal(cipherText);
 		System.out.print("Finished decryption: ");
 		System.out.println(new String(newPlainText, "UTF8")+ " is the decrypted ciphertext!");
